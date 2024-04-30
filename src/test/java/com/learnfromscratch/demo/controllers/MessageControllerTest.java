137cc0530ccb468cc5a276db8ccb321a0433de82
gpawel <gpawel@gmail.com>
2024-04-30 08:55:34 -0400

    init-impl; added unit test for the message controller and required dependencies.

---
+++ src/test/java/com/learnfromscratch/demo/controllers/MessageControllerTest.java
@@ -0,0 +1,28 @@
+package com.learnfromscratch.demo.controllers;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+import org.springframework.test.web.servlet.MockMvc;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@ExtendWith(SpringExtension.class)
+@SpringBootTest
+@AutoConfigureMockMvc
+class MessageControllerTest {
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @Test
+    void testMessage() throws Exception {
+        mockMvc.perform(get("/message"))
+                .andExpect(status().isOk())
+                .andExpect(content().string("Hi from backend!!"));
+    }
+}
